15855542670936a68b9ff077f875126467bc09ae
okdavekk <okdavekk@gmail.com>
2022-06-13 16:59:50 -0400

    Commiting to see if it works after loading templates.

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -14,36 +14,82 @@
 
 
 
-const inquirer = require('inquirer');
-const fs = require('fs');
-const { inherits } = require('util');
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const { inherits } = require('util');
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'checkbox',
-      name: 'stack',
-      message: 'What languages do you know?',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
-    },
-    {
-      type: 'list',
-      name: 'contact',
-      message: 'What is your preferred method of communication?',
-      choices: ['email', 'phone', 'telekinesis'],
-    },
-  ])
-  .then((data) => {
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       name: 'title',
+//       message: 'What is the title of your project?',
+//     },
+//     {
+//       type: 'checkbox',
+//       name: 'stack',
+//       message: 'What languages do you know?',
+//       choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+//     },
+//     {
+//       type: 'list',
+//       name: 'contact',
+//       message: 'What is your preferred method of communication?',
+//       choices: ['email', 'phone', 'telekinesis'],
+//     },
+//   ])
+//   .then((data) => {
+//     const filename = `${data.title.toLowerCase().split(' ').join('')}.json`;
+
+//     fs.writeFile(filename, JSON.stringify(data, null, 2), (err) =>
+//       err ? console.log(err) : console.log('Success!')
+//     );
+//   });
+
+// //   init()
+
+
+
+
+const { prompt } = require('inquirer');
+const utils = require('./utils.js');
+const questions = require('./questions.js');
+
+const init = async () => {
+  let result;
+  const data = await prompt(questions);
+  if (data.contact === "phone" || data.contact === "email") {
     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
 
-    fs.writeFile(filename, JSON.stringify(data, null, 2), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-  });
+    const information = JSON.parse(utils.readFromFile(filename)) || [];
 
-//   init()
+    information.push(data);
+
+    utils.writeToFile(filename, information);
+
+    const { confirm } = await prompt([
+      {
+        type: 'confirm',
+        message: 'Would you like to enter your contact info?',
+        name: 'confirm'
+      }
+    ]);
+
+    if (!confirm) process.exit();
+
+    result = await prompt([
+      {
+        type: 'input',
+        name: data.contact,
+        message: `What is your ${data.contact}`
+      }
+    ]);
+
+    if (result) {
+      information[information.length - 1] = { ...data, ...result };
+      utils.writeToFile(filename, information);
+    }
+  }
+};
+
+init();
--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -1,20 +1,42 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require('fs');
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const writeToFile = (filename, data) => {
+  fs.writeFile(filename, JSON.stringify(data, null, 2), (err) => {
+    if (err) console.log(err);
+  });
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const readFromFile = (filename) => {
+  return fs.readFileSync(filename, 'utf8');
+};
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+module.exports = {
+  writeToFile,
+  readFromFile
+};
 
-`;
-}
 
-module.exports = generateMarkdown;
+
+
+
+
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
+
+// `;
+// }
+
+// module.exports = generateMarkdown;
